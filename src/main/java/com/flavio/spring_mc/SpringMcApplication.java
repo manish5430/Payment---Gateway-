d1a0a2b346fb20fdb1e89a0add48b2bb7527b76f
Flavio Santos <flavio.pasantos@outlook.com>
2023-05-28 11:39:35 -0300

    Initial commit

---
+++ src/main/java/com/flavio/spring_mc/SpringMcApplication.java
@@ -0,0 +1,13 @@
+package com.flavio.spring_mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flavio/spring_mc/SpringMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flavio.spring_mc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
