f1f2e1a7ec8a84d42cc8c2fbb11b041bbe334d65
Flavio Santos <flavio.pasantos@outlook.com>
2023-06-20 17:21:22 -0300

    impl enum

---
+++ src/main/java/com/flavio/spring_mc/enuns/TypeClient.java
@@ -0,0 +1,38 @@
+package com.flavio.spring_mc.enuns;
+
+public enum TypeClient {
+    PESSOAFISICA(1, "Pessoa Fisíca"),
+    PESSOAJURIDICA(2, "Pessoa Jurídica");
+
+    private int cod;
+    private String descricacao;
+
+    private TypeClient(int cod, String descricacao) {
+        this.cod = cod;
+        this.descricacao = descricacao;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public String getDescricacao() {
+        return descricacao;
+    }
+
+    public static TypeClient toEnum(Integer cod){
+        if(cod == null){
+            return null;
+        }
+
+        for(TypeClient x : TypeClient.values()){
+            if(cod == x.getCod()){
+                return x;
+            }
+        }
+
+        throw new IllegalArgumentException("Id inválido " + cod);
+    }
+
+}
+
